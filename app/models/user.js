d7834af20185fa5c1febf2580ef772094b04bb93
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-12-10 17:24:56 +0100

    create user model

---
+++ app/models/user.js
@@ -0,0 +1,36 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    classMethods: {
+      associate: (models) => {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
