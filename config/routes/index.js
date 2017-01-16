3faf7c454628a39b174273b72eef31090274d8da
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-16 11:31:40 +0100

    refactor index route

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 router.route('/')
-  .get((req, res) => {
-    res.send({ message: 'hello' });
+  .get((request, response) => {
+    return response
+      .status(200)
+      .send({ message: 'Document Management System' });
   });
 
 module.exports = router;
