8e07e17a3d0fbe8b0f39ceb85880b58ea778277b
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-16 11:22:21 +0100

    add document controller to get all documents

--- app/controllers/document.js
+++ app/controllers/document.js
@@ -21,77 +21,23 @@ class DocumentsController {
   }
 
   /**
-   * Method getDocument to obtain a document for a specific user
+   * Method getDocuments to obtain all documents
    * @param {Object} request - request Object
    * @param {Object} response - request Object
    * @return {Object} documents Object
    */
-  static getUserDocument(request, response) {
-    model.Document.findById(request.params.id)
-      .then((document) => {
-        if (!document) return response.status(404)
-          .send({ message: `No document found with id: ${request.params.id}` });
-        return response.status(200)
-          .send(document);
-      });
-  }
+  static getDocuments(request, response) {
+    const query = {
+      limit: request.query.limit || null,
+      offset: request.query.offset || null
+    };
 
-  /**
-   * Method getUserDocument to obtain all documents for a specific user
-   * @param {Object} request - request Object
-   * @param {Object} response - request Object
-   * @return {Object} documents Object
-   */
-  static getUserDocuments(request, response) {
-    model.Document.findAll({ where: { OwnerId: request.params.id } })
+    model.Document.findAll(query)
       .then((documents) => {
-        if (!documents) return response.status(404)
-          .send({ message: `No document found with id: ${request.params.id}` });
         return response.status(200)
           .send(documents);
       });
   }
-
-  /**
-   * Method updateDocument
-   * @param {Object} request - request Object
-   * @param {Object} response - request Object
-   * @return {Object} documents Object
-   */
-  static updateDocument(request, response) {
-    model.Document.findById(request.params.id)
-      .then((document) => {
-        if (!document) return response.status(404)
-          .send({ message: `No document found with id: ${request.params.id}` });
-
-        document.update(request.body)
-          .then((updatedDocument) => {
-            return response.status(202)
-              .send(updatedDocument);
-          });
-      });
-  }
-
-  /**
-   * Method deleteDocument
-   * @param {Object} request - request Object
-   * @param {Object} response - request Object
-   * @return {Object} response Object
-   */
-  static deleteDocument(request, response) {
-    model.Document.findById(request.params.id)
-      .then((document) => {
-        if (!document) return response.status(404)
-          .send({ message: `No document found with id: ${request.params.id}` });
-
-        document.destroy()
-          .then(() => {
-            return response.status(202)
-              .send({ message: 'Document succesfully deleted' });
-          });
-      });
-  }
-
 }
 
 module.exports = DocumentsController;
