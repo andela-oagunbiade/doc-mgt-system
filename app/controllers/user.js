9877d949bc05dc5082fa1d9b1a9d6e2b0b77c81e
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-16 11:09:35 +0100

    add user controller to create a user

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,21 @@
+const jwt = require('jsonwebtoken');
 const model = require('../models');
+
+const secret = process.env.SECRET_TOKEN || 'secret';
+
+// To create text-friendly user details
+const formattedUser = (user) => {
+  const userDetails = {
+    id: user.id,
+    userName: user.userName,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    RoleId: user.RoleId,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt
+  };
+  return userDetails;
+};
 
 /**
  *
@@ -27,6 +44,37 @@ class UsersController {
       return response.status(202)
         .send(users);
     });
+  }
+
+  /**
+   * Method createUser to create a user
+   * @param {object} request - request object
+   * @param {object} response - response object
+   * @returns {Response} - response object
+   */
+  static createUser(request, response) {
+    model.User.findOne({ where: { email: request.body.email } })
+      .then((existingUser) => {
+        if (existingUser) return response.status(409)
+            .send({ message: `Unable to create user. This mail:
+              ${request.body.email} is already in use` });
+
+        model.User.create(request.body)
+          .then((newUser) => {
+            const token = jwt.sign({
+              UserId: newUser.id,
+              RoleId: newUser.RoleId
+            }, secret, { expiresIn: '2 days' });
+
+            newUser = formattedUser(newUser);
+            return response.status(201)
+              .send({ newUser, token, expiresIn: '2 days' });
+          })
+          .catch((error) => {
+            return response.status(400)
+              .send(error.errors);
+          });
+      });
   }
 }
 
