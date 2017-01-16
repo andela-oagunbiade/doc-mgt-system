f1be01382e10a12cc8047139621deb8ae7362296
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-16 11:32:20 +0100

    configure role routes

---
+++ config/routes/role.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const rolesController = require('../../app/controllers/role');
+const auth = require('../../app/controllers/authentication');
+
+router.route('/')
+  .get(auth.verifyToken, rolesController.getRoles)
+  .post(auth.verifyToken, auth.adminAccess, rolesController.createRole);
+
+router.route('/:id')
+  .get(auth.verifyToken, auth.adminAccess, rolesController.getRoles)
+  .put(auth.verifyToken, auth.adminAccess, rolesController.updateRole)
+  .delete(auth.verifyToken, auth.adminAccess, rolesController.deleteRole);
+
+module.exports = router;
