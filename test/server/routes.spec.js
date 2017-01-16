8e584eedbf5fb145492339072d61d5912274a638
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-16 11:48:09 +0100

    refactor index route test

--- test/server/routes.spec.js
+++ test/server/routes.spec.js
@@ -1,8 +1,9 @@
-const request = require('supertest');
 const server = require('../../server.js');
+const request = require('supertest')(server);
+
 
 describe('Index Route', () => {
   it('should correctly load the index route', (done) => {
-    request(server).get('/').expect(200, done);
+    request.get('/').expect(200, done);
   });
 });
