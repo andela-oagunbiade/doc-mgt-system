7de39004a0dcfdd7df0bbeb878f75b90ec052eb5
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-16 11:38:29 +0100

    configure document routes

---
+++ config/routes/document.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const documentsController = require('../../app/controllers/document');
+const auth = require('../../app/controllers/authentication');
+
+
+router.route('/')
+  .get(auth.verifyToken, documentsController.getDocuments)
+  .post(auth.verifyToken, documentsController.createDocument);
+
+router.route('/:id')
+  .get(auth.verifyToken, documentsController.getUserDocument)
+  .put(auth.verifyToken, documentsController.updateDocument)
+  .delete(auth.verifyToken, documentsController.deleteDocument);
+
+module.exports = router;
