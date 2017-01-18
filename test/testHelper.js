ac9108fe6cb1aeb76fe8a0bac3d3ec7e451c2c30
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-18 22:51:28 +0100

    add additional user and document

--- test/testHelper.js
+++ test/testHelper.js
@@ -15,8 +15,22 @@ module.exports = {
     password: '123456'
   },
 
+  testUser2: {
+    userName: 'femi',
+    firstName: 'femi',
+    lastName: 'dotexe',
+    email: 'femidotexe@gmail.com',
+    password: '123456'
+  },
+
   testDocument: {
     title: 'Test Document',
     content: 'A Model represents a table in the database. Also called a factory.'
+  },
+
+  testDocument2: {
+    title: 'Other Document',
+    content: 'Buildpacks are scripts that are run when your app is deployed.',
+    access: 'private'
   }
 };
