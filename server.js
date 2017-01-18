fce38e33a8f9c573a725a173373407bf456b7ffd
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-18 08:53:58 +0100

    import role routes into server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const parser = require('body-parser');
 const homeRoute = require('./config/routes/index');
 const docRoutes = require('./config/routes/document');
 const userRoutes = require('./config/routes/user');
+const roleRoutes = require('./config/routes/role');
 
 const port = process.env.PORT || 3000;
 
@@ -16,6 +17,7 @@ app.use(parser.json());
 app.use('/', homeRoute);
 app.use('/documents', docRoutes);
 app.use('/users', userRoutes);
+app.use('/roles', roleRoutes);
 
 // Start Server
 app.listen(port, () => {
