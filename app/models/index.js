1f59a34790d82bb007f1afd8bf878ba49a00d167
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-01-19 12:07:28 +0100

    update production envinronment config

--- app/models/index.js
+++ app/models/index.js
@@ -11,7 +11,7 @@ const config = require('../../config/config.json')[env];
 const db = {};
 
 const sequelize = (config.use_env_variable) ?
-  new Sequelize(process.env[config.use_env_variable]) :
+  new Sequelize(process.env.DATABASE_URL) :
   new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, config);
 
 
