76690ec3ab902756ea04462926eec5f84b99eebc
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-06-12 14:49:06 +0100

    chore(redux-store): configure redux store and connect it to application

---
+++ src/store/configureStore.js
@@ -0,0 +1,13 @@
+/*  eslint-disable require-jsdoc */
+
+import { createStore, applyMiddleware } from 'redux';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import rootReducer from '../reducers';
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
+}
