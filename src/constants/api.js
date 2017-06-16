1647e0956597e02d48ff440bafa83b28dc609429
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-06-16 11:21:28 +0100

    chore(constants): update action and add api constants

---
+++ src/constants/api.js
@@ -0,0 +1,8 @@
+const apiConstants = {
+  ROOT_API_PATH: 'api/v1',
+  DOCUMENTS_PATH: 'api/v1/documents',
+  USERS_PATH: 'api/v1/users',
+  ROLES_PATH: 'api/v1/users'
+};
+
+export default apiConstants;
