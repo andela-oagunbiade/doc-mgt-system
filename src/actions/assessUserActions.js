335df2c34c7aa5cdda300dff811d9c44f048ed93
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2018-02-14 18:42:44 +0100

    add action to send SMS

--- src/actions/assessUserActions.js
+++ src/actions/assessUserActions.js
@@ -7,3 +7,28 @@ export const createAssessUser = (user) => {
 export const getAssessUser = () => {
   return axios.get(`/api/v1/assessUsers`);
 };
+
+export const SMS = (info) => {
+  const requestConfig = {
+    baseURL: SmsServer.baseUrl,
+    auth: {
+      username: SmsServer.username,
+      password: SmsServer.password
+    }
+  };
+  const request = axios.create(requestConfig);
+
+  return request
+    .post('/sms/1/text/single',
+    {
+      from: 'Assessment App',
+      to: '+2348167304897',
+      text: 'Test SMS from assessment app'
+    })
+    .then((response) => {
+      console.log('response======>', response);
+    })
+    .catch((error) => {
+      console.log('Error======>', error);
+    });
+};
