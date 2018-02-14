3379283b60cafd23f3adff5c0cf2fb602e52a342
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2018-02-14 18:31:44 +0100

    add action to view users

--- src/actions/assessUserActions.js
+++ src/actions/assessUserActions.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 export const createAssessUser = (user) => {
-  return axios.post(`/api/v1/assessUsers`, user)
-    .then((response) => {
-      return response;
-    }).catch((error) => {
-      return Promise.reject(error);
-    });
+  return axios.post(`/api/v1/assessUsers`, user);
 };
 
+export const getAssessUser = () => {
+  return axios.get(`/api/v1/assessUsers`);
+};
